f61d47640228259b8d4e17b5df91e62c0e4d1a7e
Jae-Yong Choi <jayong93@gmail.com>
2018-04-18 00:13:31 +0900

    nmt 모델에 맞춰서 생성되는 train, test data set 이름 조절

--- seperate_dataset.py
+++ seperate_dataset.py
@@ -6,8 +6,8 @@ import random
 def add_arguments(parser):
     parser.add_argument("title", type=argparse.FileType('r', encoding='utf-8'), help="text file which has titles")
     parser.add_argument("comment", type=argparse.FileType('r', encoding='utf-8'), help="text file which has comments")
-    parser.add_argument("--title_prefix", default='title')
-    parser.add_argument("--comment_prefix", default='comment')
+    parser.add_argument("--title_suffix", default='title')
+    parser.add_argument("--comment_suffix", default='comment')
     return parser
 
 
@@ -20,10 +20,10 @@ if __name__ == '__main__':
     random.shuffle(data_set)
 
     train_len = int(len(data_set)*0.7)
-    title_train = open(args.title_prefix + '_train', 'w', encoding='utf-8')
-    comment_train = open(args.comment_prefix + '_train', 'w', encoding='utf-8')
-    title_test = open(args.title_prefix + '_test', 'w', encoding='utf-8')
-    comment_test = open(args.comment_prefix + '_test', 'w', encoding='utf-8')
+    title_train = open('train.' + args.title_suffix, 'w', encoding='utf-8')
+    comment_train = open('train.' + args.comment_suffix, 'w', encoding='utf-8')
+    title_test = open('test.' + args.title_suffix, 'w', encoding='utf-8')
+    comment_test = open('test.' + args.comment_suffix, 'w', encoding='utf-8')
     for (title, comment) in data_set[:train_len]:
         title_train.write(title+'\n')
         comment_train.write(comment+'\n')
